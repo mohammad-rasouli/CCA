405a1c616ad606c4a2e93fe0d9939c76b46cfc24
Mohammad Rasouli <109364478+mohammad-rasouli@users.noreply.github.com>
2024-06-03 09:42:42 +0330

    Add files via upload
    Using BiomaRt package to convert ENSG IDs of Transcriptome dataset to Gene Symbols

---
+++ ENSGtoGenesymbol.R
@@ -0,0 +1,14 @@
+library(biomaRt)
+
+transcript <- read.csv("C:/Users/mohammad/OneDrive/Desktopd/datasets/RNA_filtered.csv", header = TRUE)
+ensg <- colnames(df) # taking ensg ids and save them as a list
+
+ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl") #creating biomart object
+conversion <- getBM(attributes = c('ensembl_gene_id', 'hgnc_symbol'),
+                    filters = 'ensembl_gene_id',
+                    values = ensg,
+                    mart = ensembl)
+
+#change and save the converted col names 
+colnames(transcript) <- conversion$hgnc_symbol[match(colnames(transcript), conversion$ensembl_gene_id)]
+write.csv(transcript, "transcriptome_genesymbol.csv", row.names = FALSE)
